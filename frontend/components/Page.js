2473a234ef6dd3201c5f03a5242969e5c4aa1753
ArtyomNegev <you@example.com>
2020-08-02 18:33:17 +0200

    -Ajout du Meta -Ajout du favicon -Début de theme (bug?)

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
 import Header from "./Header";
+import Meta from "./Meta";
+import styled, { ThemeProvider, injectGlobal} from "styled-components"
+
+const theme = {
+	primary: "#673Ab7",
+	second: "#ebebeb",
+	text: "#494949",
+	textWhite: "white",
+	maxWidth: "1200px",
+	boxS: "0 10px 20px 0 rgba(0, 0, 0, 0.08)"
+};
+
+const StyledPage = styled.div`
+	background: ${props => props.theme.second}; 
+	color: ${props => props.theme.text}; 
+`;
+
+const StyledPageContent = styled.div`
+	background: ${props => props.theme.primary}; 
+	max-width: ${props => props.theme.maxWidth};
+	margin: 0 auto;
+	padding: 1.5rem; 
+`;
 
 class Page extends Component {
 	render() {
 		return (
-			<div>
+			<StyledPage>
+				<Meta />
 				<Header />
-				{ this.props.children }
-			</div>
+				<StyledPageContent>
+					{ this.props.children }
+				</StyledPageContent>
+			</StyledPage>
 		)
 	}
 }
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,14 +1,12 @@
-import App, { Container } from "next/app" ;
+import App from "next/app" ;
 import Page from "../components/Page";
 class monApp extends App {
 	render() {
 		const { Component, pageProps } = this.props;
 		return (
-			<Container>
-				<Page>
-					<Component {...pageProps} />
-				</Page>
-			</Container>
+			<Page>
+				<Component {...pageProps} />
+			</Page>
 		)
 	}
 }
