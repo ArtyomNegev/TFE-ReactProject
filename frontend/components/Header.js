f6b275698353830efc4d45e687f0b968008b1da3
Artyom <bergerromain151@gmail.com>
2020-08-07 17:51:57 +0200

    -Ajout de la barre de chargement -Modification du CSS de la barre de chargement

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,6 +1,19 @@
 import Navbar from "./Navbar";
 import styled from "styled-components";
 import Link from "next/link";
+import Router from "next/router";
+import NProgress from "nprogress";
+
+Router.onRouteChangeStart = () => {
+	NProgress.start();
+}
+Router.onRouteChangeComplete = () => {
+	NProgress.done();
+}
+Router.onRouteChangeError = () => {
+	console.log("Erreur lors du chargement de la page");
+	NProgress.done();
+}
 
 const StyledHeader= styled.header`
 	.navbar {
