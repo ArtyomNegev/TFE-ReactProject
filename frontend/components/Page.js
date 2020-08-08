d573d8b680581c52525859fa9b35a931c61419a8
Artyom <bergerromain151@gmail.com>
2020-08-08 17:11:38 +0200

    -Correction d'erreur lors du rafraichissement de la page

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,34 +1,7 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import Meta from "./Meta";
-import styled, { ThemeProvider, createGlobalStyle} from "styled-components"
-
-const GlobalStyle = createGlobalStyle`
-	@font-face {
-		font-family: "robotoregular";
-		src: url("/static/roboto-regular.webfont.woff2") format ("woff2");
-		font-weight: normal;
-		font-style: normal;
-	}
-	html {
-		font-family: "robotoregular";
-		box-sizing: border-box;
-		font-size: 10px; /* Forcer la taille par defaut */
-	}
-	*, *:before, *:after {
-		box-sizing: inherit;
-	}
-	body {
-		padding: 0;
-		margin: 0;
-		font-size: 1.5rem;
-		line-height: 1.5;
-	}
-	a {
-		text-decoration: none;
-		color: ${props => props.theme.text};
-	}
-`;
+import styled, { ThemeProvider } from "styled-components"
 
 const theme = {
 	primary: "#09b5a6",
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,34 @@
 import App from "next/app" ;
 import Page from "../components/Page";
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+	@font-face {
+		font-family: "robotoregular";
+		src: url("..\static\roboto-regular-webfont.woff2") format ("woff2");
+		font-weight: normal;
+		font-style: normal;
+	}
+	html {
+		font-family: "robotoregular";
+		box-sizing: border-box;
+		font-size: 10px; /* Forcer la taille par defaut */
+	}
+	*, *:before, *:after {
+		box-sizing: inherit;
+	}
+	body {
+		padding: 0;
+		margin: 0;
+		font-size: 1.5rem;
+		line-height: 1.5;
+	}
+	a {
+		text-decoration: none;
+		color: ${props => props.theme.text};
+	}
+`;
+
 class monApp extends App {
 	render() {
 		const { Component, pageProps } = this.props;
